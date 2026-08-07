6c0bcfbd45b2c34354bf70ef401f492a84d38bf5
hamza-javaid117 <hjavaid797@gmail.com>
2026-08-07 06:27:39 +0500

    timeout incresed

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
-  timeout: 15000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
